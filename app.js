aa307b7375dca96381df31e32871889edd5e40b4
Assaf Fogelman <assaf.fogelman@gmail.com>
2022-12-25 14:22:59 +0200

    integrating react and making it RTL 	modified:   .vscode/settings.json 	modified:   app.js

--- app.js
+++ app.js
@@ -8,12 +8,22 @@ const cardsRouter = require("./Routes/Cards/cardsRouter");
 const chalk = require("chalk");
 const morgan = require("morgan");
 const cors = require("cors");
+const path = require("node:path");
 
 app.use(morgan(chalk.cyan(":method :url :status :response-time ms")));
 app.use(cors());
 app.use(express.json());
 app.use("/api/users", usersRouter);
 app.use("/api/cards", cardsRouter);
+
+/* React*/
+
+//gives access to the build directory
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 const PORT = 8182;
 app.listen(PORT, () =>
